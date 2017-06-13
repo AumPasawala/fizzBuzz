bebc79246a4b3dadd0f03cc71370535d7989a732
Aum Pasawala <apasawala@aums-macbook-air.local>
2017-06-13 09:58:10 -0500

    initial comit

--- Fizz Buzz/ViewController.swift
+++ Fizz Buzz/ViewController.swift
@@ -10,16 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var myNumberLabel: UILabel!
+    @IBOutlet var longPress: UILongPressGestureRecognizer!
+    var number = 0
+
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        myNumberLabel.text = "\(number)"
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func screenTapped(_ sender: UITapGestureRecognizer)
+    {
+        print("screen tapped")
+        number += 1
+        if number % 15 == 0
+        {
+            view.backgroundColor = UIColor.yellow
+            myNumberLabel.text = "FIZZ BUZZ"
+            myNumberLabel.font = myNumberLabel.font.withSize(67)
+
+        }
+        else if number % 3 == 0
+        {
+            print("FIZZ")
+            view.backgroundColor = UIColor.blue
+            myNumberLabel.text = "FIZZ"
+        }
+        
+
+            
+            else if number % 5 == 0
+    
+            {
+                print("BUZZ")
+                view.backgroundColor = UIColor.red
+                myNumberLabel.text = "BUZZ"
+
+            }
+        else
+        {
+        myNumberLabel.text = "\(number)"
+        view.backgroundColor = UIColor.black
+        myNumberLabel.font = myNumberLabel.font.withSize(100)
+        }
+
     }
+
+
 
 
 }
-
